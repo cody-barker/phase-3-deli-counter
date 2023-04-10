b8e4c56bada55ea9c601328fe41f699e75ecf3ba
Cody <codybarker.or@gmail.com>
2023-04-10 16:09:36 -0700

    s

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,8 +4,7 @@ deli = []
 
 def line(deli)
     if deli.empty? then puts "The line is currently empty."
-    else 
-        current_line = "The line is currently:"
+    else current_line = "The line is currently:"
         deli.each.with_index(1) do |customer, i|
             current_line << " #{i}. #{customer}"
         end
@@ -14,7 +13,7 @@ def line(deli)
 end
 
 def take_a_number(deli, customer)
-    puts "Welcome, #{customer}. You are number #{deli.length} in line"
     deli << customer
+    puts "Welcome, #{customer}. You are number #{deli.length} in line"
 end
 
